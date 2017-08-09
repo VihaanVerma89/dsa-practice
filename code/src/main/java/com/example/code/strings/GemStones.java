f2c3aa3a992a96e4c62256619c808dee9731697b
Vihaan Verma <vihaanverma21@gmail.com>
2017-08-09 21:38:28 +0530

    https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem

---
+++ code/src/main/java/com/example/code/strings/GemStones.java
@@ -0,0 +1,31 @@
+package com.example.code.strings;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * Created by vihaan on 05/08/17.
+ */
+
+public class GemStones {
+
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Set<Character> set = convertToSet(scan.next());
+        for(int i=1; i<t ;i++){
+            set.retainAll(convertToSet(scan.next()));
+        }
+        System.out.print(set.size());
+    }
+
+
+    public static Set<Character> convertToSet(String s) {
+        Set<Character> set = new HashSet<Character>(26);
+        for (char c : s.toCharArray())
+            set.add(Character.valueOf(c));
+        return set;
+    }
+}
---
+++ code/src/main/java/com/example/code/strings/SherlockAndAnagrams.java
@@ -0,0 +1,119 @@
+package com.example.code.strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by vihaan on 09/08/17.
+ */
+
+public class SherlockAndAnagrams {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        String input;
+        int result;
+
+//        String input = "abba";
+//
+//        result = getAnagramCount(input);
+//        System.out.println(result);
+
+//
+        for (int i = 0; i < t; i++) {
+            input = scanner.next();
+            result = getAnagramCount(input);
+            System.out.println(result);
+        }
+    }
+
+    public static int getAnagramCount(String input) {
+        int count = 0;
+        ArrayList<String> subStrings = getSubStrings(input);
+
+        String s1, s2;
+        for (int i = 0; i < subStrings.size(); i++) {
+            s1 = subStrings.get(i);
+//            for(int j=i+1; j< subStrings.size()-1; j++)
+            for (int j = i + 1; j < subStrings.size(); j++) {
+                s2 = subStrings.get(j);
+                if (isAnagram(s1, s2)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static ArrayList<String> getSubStrings(String input) {
+        ArrayList<String> subStrings = new ArrayList<>();
+        String subString;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+//                System.out.println("i:" + i + ",j:" + j);
+                subString = input.substring(i, j);
+//                System.out.println(subString);
+                subStrings.add(subString);
+            }
+        }
+
+        return subStrings;
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        boolean result = true;
+
+        if (s1.length() == s2.length()) {
+            HashMap<Character, Integer> s1freq = getCharFrequency(s1);
+            HashMap<Character, Integer> s2freq = getCharFrequency(s2);
+
+            Iterator i = s1freq.entrySet().iterator();
+
+            while (i.hasNext()) {
+                Map.Entry<Character, Integer> pair = (Map.Entry) i.next();
+                char s1Key = pair.getKey();
+                int s1Value = pair.getValue();
+
+                int s2Value;
+
+                if (s2freq.containsKey(s1Key)) {
+                    s2Value = s2freq.get(s1Key);
+                    if (s1Value != s2Value) {
+                        result = false;
+                        break;
+                    }
+                } else {
+                    result = false;
+                    break;
+                }
+
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    public static HashMap<Character, Integer> getCharFrequency(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            Integer value = hashMap.get(c);
+            if (value != null) {
+                value++;
+                hashMap.put(c, value);
+            } else {
+                hashMap.put(c, 1);
+            }
+        }
+        return hashMap;
+    }
+
+
+}
