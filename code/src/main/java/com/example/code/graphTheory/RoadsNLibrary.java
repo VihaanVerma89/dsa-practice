f8f559ed70560fd2f540aefb9614f7fd609cfd0c
Vihaan Verma <vihaanverma21@gmail.com>
2017-11-02 13:05:00 +0530

    last commit

---
+++ code/src/main/java/com/example/code/graphTheory/RoadsNLibrary.java
@@ -0,0 +1,28 @@
+package com.example.code.graphTheory;
+
+/**
+ * Created by vihaan on 01/11/17.
+ */
+
+public class RoadsNLibrary {
+
+    public static void main(String[] args) {
+        RoadsNLibrary roadsNLibrary = new RoadsNLibrary();
+        roadsNLibrary.input();
+        roadsNLibrary.process();
+    }
+
+    public void input() {
+
+    }
+
+    public void testInput(){
+
+    }
+
+    public void process()
+    {
+
+    }
+
+}
