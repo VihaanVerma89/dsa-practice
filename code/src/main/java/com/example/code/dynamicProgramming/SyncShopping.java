c36cd04c79ccaa0a18552405e93b23d334eb2be0
Vihaan <vihaanverma21@gmail.com>
2017-11-14 21:44:30 +0530

    syncshopping

--- code/src/main/java/com/example/code/dynamicProgramming/SyncShopping.java
+++ code/src/main/java/com/example/code/dynamicProgramming/SyncShopping.java
@@ -1,12 +1,11 @@
 package com.example.code.dynamicProgramming;
 
-import android.support.annotation.NonNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 
@@ -59,35 +58,61 @@ public class SyncShopping {
             line = br.readLine();
             parts = line.split(" ");
 
-            int start = Integer.parseInt(parts[0]);
-            int end = Integer.parseInt(parts[1]);
+            int start = Integer.parseInt(parts[0])-1;
+            int end = Integer.parseInt(parts[1])-1;
             int len = Integer.parseInt(parts[2]);
 
             edges[start].add(new Edge(start, end, len));
-            edges[end].add(new Edge(start, end, len));
+            edges[end].add(new Edge(end, start, len));
+        }
+
+        long[][] dist = dijkstras(0, edges,cityMask);
+
+        long ans = INF;
+        for(int mask1=0;mask1< 1<<k; ++mask1)
+        {
+            int complement = (1<<k)-1-mask1;
+            for(int mask2=complement;mask2< 1<<k; mask2 =(mask2+1)|complement)
+            {
+                long time = Math.max(dist[n-1][mask1], dist[n-1][mask2]);
+                ans = Math.min(ans,time);
+            }
         }
 
 
+        System.out.println(ans);
     }
 
     private void solve() {
         dijkstras(0, edges, cityMask);
     }
 
-    private void dijkstras(int start, ArrayList<Edge>[] edges, int[] cityMask) {
+    static final long INF = 1000000000000000000L;
+    private long[][] dijkstras(int start, ArrayList<Edge>[] edges, int[] cityMask) {
 
-        int distance[][] = new int[edges.length][1 << k];
+        long distance[][] = new long[edges.length][1 << k];
+
+        for(long [] ar: distance)
+        {
+            Arrays.fill(ar,INF);
+        }
 
         Node startingNode = new Node(start, cityMask[start], 0);
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(startingNode);
 
+        distance[start][cityMask[start]]=0;
         while (!pq.isEmpty()) {
             Node node = pq.poll();
 
+            if(node.distance != distance[node.id][node.mask])
+            {
+                continue;
+            }
+
             for (Edge e : edges[node.id]) {
-                int nDistance = distance[e.start][node.mask] + e.len;
+                long nDistance = distance[e.start][node.mask] + e.len;
                 int newMask = node.mask | cityMask[e.end];
                 if (nDistance < distance[e.end][newMask]) {
                     distance[e.end][newMask] = nDistance;
@@ -95,12 +120,16 @@ public class SyncShopping {
                 }
             }
         }
+
+        return distance;
     }
 
     class Node implements Comparable<Node> {
-        int id, mask, distance;
+        int id;
+        int mask;
+        long distance;
 
-        public Node(int id, int mask, int distance) {
+        public Node(int id, int mask, long distance) {
             this.id = id;
             this.mask = mask;
             this.distance = distance;
@@ -122,7 +151,7 @@ public class SyncShopping {
             this.mask = mask;
         }
 
-        public int getDistance() {
+        public long getDistance() {
             return distance;
         }
 
@@ -131,7 +160,7 @@ public class SyncShopping {
         }
 
         @Override
-        public int compareTo(@NonNull Node node) {
+        public int compareTo(Node node) {
             return Long.compare(distance, node.distance);
         }
     }
