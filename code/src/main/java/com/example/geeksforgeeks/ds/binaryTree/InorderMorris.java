c98044b33880704dee03a019b8f2fa9386ac7c3b
Vihaan <vihaanverma21@gmail.com>
2017-12-03 21:18:51 +0530

    morris algo

---
+++ code/src/main/java/com/example/geeksforgeeks/ds/binaryTree/InorderMorris.java
@@ -0,0 +1,116 @@
+package com.example.geeksforgeeks.ds.binaryTree;
+
+/**
+ * Created by vihaanverma on 03/12/17.
+ */
+
+public class InorderMorris {
+
+    public static void main(String[] args) {
+
+        Node leftNode, rightNode;
+        Node rootNode = new Node(10);
+
+        leftNode = new Node(12);
+        rightNode = new Node(15);
+
+        rootNode.addLeftNode(leftNode);
+        rootNode.addRightNode(rightNode);
+
+        leftNode = new Node(25);
+        rightNode = new Node(30);
+
+        rootNode.getLeftNode().addLeftNode(leftNode);
+        rootNode.getLeftNode().addRightNode(rightNode);
+
+        leftNode = new Node(36);
+        rootNode.getRightNode().addLeftNode(leftNode);
+
+        inorderWithStack(rootNode);
+    }
+
+    private static void inorderWithStack(Node rootNode) {
+
+        while(rootNode!=null)
+        {
+            Node tempNode, rightNode, leftNode;
+
+            leftNode = rootNode.getLeftNode();
+            if(leftNode!=null)
+            {
+                tempNode = leftNode;
+                while(tempNode.getRightNode()!=null && tempNode.getRightNode()!=rootNode)
+                {
+                    tempNode = tempNode.getRightNode();
+                }
+
+
+                if(tempNode.getRightNode()==null)
+                {
+                    tempNode.addRightNode(rootNode);
+                    rootNode=rootNode.getLeftNode();
+                }
+                else {
+                    System.out.print(rootNode.getValue()+"=");
+                    rootNode = rootNode.getRightNode();
+                    tempNode.setRightNode(null);
+                }
+
+            }else{
+                System.out.print(rootNode.getValue()+"=");
+                rootNode = rootNode.getRightNode();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
