f918b09cdaea2d4fbd41f4f357ff1d63355c63ac
Vihaan Verma <vihaan.verma@paytm.com>
2020-10-05 09:03:35 +0530

    maxArea 2nd time

--- lib/src/main/java/com/example/lib/topics/twoPointerApproach/ContainerWithMostWater.kt
+++ lib/src/main/java/com/example/lib/topics/twoPointerApproach/ContainerWithMostWater.kt
@@ -3,23 +3,46 @@ package com.example.lib.topics.twoPointerApproach
 import kotlin.math.max
 
 
-fun maxArea(height: IntArray): Int {
-    var maxArea = 0
-    var l = 0
-    var r = height.size - 1
+class ContainerWithMostWater {
 
-    while (l < r) {
+    // 5th oct , 2020
+    fun maxArea(a: IntArray): Int {
+        var l = 0
+        var r = a.size - 1
+        var maxA = 0
+        while (l < r) {
+            val ca = (r - l) * Math.min(a[l], a[r])
+            maxA = Math.max(maxA, ca)
 
-        maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
-        if (height[l] < height[r]) {
-            l++
-        } else {
-            r--
+            if (a[l] < a[r]) {
+                l++
+            } else {
+                r--
+            }
+
         }
+        return maxA
     }
-    return maxArea
-}
 
+    fun maxArea1(height: IntArray): Int {
+
+        var maxArea = 0
+        var l = 0
+        var r = height.size - 1
+
+        while (l < r) {
+
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++
+            } else {
+                r--
+            }
+        }
+        return maxArea
+    }
+
+}
 
 fun main() {
 
