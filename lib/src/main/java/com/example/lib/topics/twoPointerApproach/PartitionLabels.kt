b0cd90d0ebad3be24742337b0d6f01082b659672
Vihaan Verma <vihaan.verma@paytm.com>
2020-11-06 09:33:02 +0530

    partition label

---
+++ lib/src/main/java/com/example/lib/topics/twoPointerApproach/PartitionLabels.kt
@@ -0,0 +1,73 @@
+package com.example.lib.topics.twoPointerApproach
+
+class PartitionLabels {
+    fun partitionLabels(s: String): List<Int> {
+        val lastPositionArray = IntArray(26) {
+            0
+        }
+
+        for (iv in s.withIndex()) {
+            val index = iv.index
+            val value = iv.value
+            val arrayIndex = value - 'a'
+            lastPositionArray[arrayIndex] = index
+        }
+
+        var lastIndex = 0
+        var partitionStart = 0
+        var ans = arrayListOf<Int>()
+        for (iv in s.withIndex()) {
+            val index = iv.index
+            val value = iv.value
+            val arrayIndex = value - 'a'
+            val charLastIndex = lastPositionArray[arrayIndex]
+            if (charLastIndex > lastIndex) {
+                lastIndex = charLastIndex
+            }
+
+            if (lastIndex == index) {
+                var i = index - partitionStart + 1
+                ans.add(i)
+                partitionStart = index + 1
+            }
+        }
+        return ans
+    }
+
+    //    fun partitionLabels(s: String): List<Int> {
+//        val last = IntArray(26) {
+//            -1
+//        }
+//
+//        for (civ in s.withIndex()) {
+//            val i = civ.value - 'a'
+//            last[i] = civ.index
+//        }
+//
+//        var j = 0
+//        var anchor = 0
+//        val ans = arrayListOf<Int>()
+//
+//        for (civ in s.withIndex()) {
+//            val index = civ.index
+//            val v = civ.value - 'a'
+//            val lastPosition = last[v]
+//            j = Math.max(j, lastPosition)
+//            if (index == j) {
+//                ans.add(index - anchor + 1)
+//                anchor = index + 1
+//            }
+//        }
+//        return ans
+//    }
+
+
+}
+
+fun main() {
+    val p = PartitionLabels()
+    val s = "abccaddbeffe"
+    val partitionLabels = p.partitionLabels(s)
+    val joinToString = partitionLabels.joinToString(",")
+    println(joinToString)
+}
