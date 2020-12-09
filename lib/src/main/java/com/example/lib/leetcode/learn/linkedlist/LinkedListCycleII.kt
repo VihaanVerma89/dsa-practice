cd4d0f4992d2dfbc6f3796668284886309893993
Vihaan Verma <vihaan.verma@paytm.com>
2020-12-09 16:35:51 +0530

    IntersectionofTwoLinkedLists

---
+++ lib/src/main/java/com/example/lib/leetcode/learn/linkedlist/LinkedListCycleII.kt
@@ -0,0 +1,23 @@
+package com.example.lib.leetcode.learn.linkedlist
+
+class LinkedListCycleII {
+
+    fun detectCycle(head: ListNode?): ListNode? {
+        var node: ListNode? = null
+        head?.let {
+            var i = head
+            var j = head.next
+            while (j != null && j != i) {
+                i = i?.next
+                j = j?.next?.next
+            }
+            node = j
+        }
+        return node
+    }
+}
+
+fun main() {
+
+
+}
