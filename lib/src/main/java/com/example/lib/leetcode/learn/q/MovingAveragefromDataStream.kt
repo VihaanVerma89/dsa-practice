a1173943d3edf79a7c2c9c4ea2f0b528a2f6e04f
Vihaan <vihaan@sharechat.co>
2021-01-16 10:33:06 +0530

    MovingAveragefromDataStream

---
+++ lib/src/main/java/com/example/lib/leetcode/learn/q/MovingAveragefromDataStream.kt
@@ -0,0 +1,39 @@
+package com.example.lib.leetcode.learn.q
+
+import java.util.*
+
+//https://leetcode.com/explore/learn/card/queue-stack/228/first-in-first-out-data-structure/1368/
+class MovingAverage(val size: Int) {
+
+    /*
+    * 0 1 2
+    *   1 2 3
+    *     3 4 5
+    *       4 5 6
+    *         5 6 7
+    * */
+    val q: Queue<Int> = LinkedList<Int>()
+    var offerCount = 0
+    fun next(`val`: Int): Double {
+        offerCount++
+        if (q.size == size) {
+            // q is full
+            val poll = q.poll()
+        }
+
+        // q is not full
+        q.offer(`val`)
+
+        var sum = 0.toDouble()
+        for (e in q) {
+            sum += e
+        }
+        var d = size
+        if (offerCount < size) {
+            d = offerCount
+        }
+        val avg = sum / d
+        return avg
+    }
+
+}
