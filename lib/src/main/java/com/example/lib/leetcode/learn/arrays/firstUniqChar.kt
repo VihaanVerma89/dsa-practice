a73ad7a66e69361d44b1d03a1f655d893e5a9f2d
vihaan verma <vihaanverma21@gmail.com>
2023-03-26 13:03:44 +0530

    firstUniqChar

---
+++ lib/src/main/java/com/example/lib/leetcode/learn/arrays/firstUniqChar.kt
@@ -0,0 +1,24 @@
+package com.example.lib.leetcode.learn.arrays
+
+class firstUniqChar {
+
+    fun firstUniqChar(s: String): Int {
+
+        val hmap = hashMapOf<Char, Int>()
+
+        for (c in s) {
+            hmap.put(c, hmap.getOrDefault(c, 0) + 1)
+        }
+
+        for( iv in s.withIndex())
+        {
+            val value = hmap.get(iv.value)
+            if(value ==1){
+                return iv.index
+            }
+        }
+
+        return -1
+
+    }
+}
