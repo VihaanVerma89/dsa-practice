b726bc2fd6d01567e7f865aa7fc3c5be1f346740
vihaan verma <vihaanverma21@gmail.com>
2023-09-09 14:15:49 +0530

    zigzagLevelOrder

--- lib/src/main/java/com/example/lib/leetcode/trees/ZigzagLevelOrder.kt
+++ lib/src/main/java/com/example/lib/leetcode/trees/ZigzagLevelOrder.kt
@@ -1,14 +1,58 @@
-//package com.example.lib.leetcode.trees
-//
-//import com.example.lib.leetcode.trees.TreeNode
-//
-//class zigzagLevelOrder {
-//
-//    fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
-//
-//
-//
-//
-//    }
-//
-//}
+package com.example.lib.leetcode.trees
+
+import com.example.lib.leetcode.trees.TreeNode
+import java.util.*
+import kotlin.collections.ArrayList
+
+class zigzagLevelOrder {
+
+    fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+        val r = arrayListOf<ArrayList<Int>>()
+
+        if (root != null) {
+            val q = LinkedList<TreeNode>()
+            var node = root
+            q.add(node)
+            var d = 0
+            val s = Stack<TreeNode>()
+            while (q.isNotEmpty()) {
+
+                val l2r = arrayListOf<Int>()
+                val r2l = arrayListOf<Int>()
+
+                for (i in 0 until q.size) {
+                    node = q.remove()
+                    if (d % 2 == 0) {
+                        // l -> r traversal
+                        if (node != null) {
+                            l2r.add(node.`val`)
+                        }
+                    } else {
+                        // l <-r traversal
+                        s.push(node)
+                    }
+                    node?.left?.let { q.add(it) }
+                    node?.right?.let { q.add(it) }
+                }
+
+                // level traversal complete
+                if (d % 2 == 0) {
+                    // l -> r
+                    r.add(l2r)
+                } else {
+                    // l <- r
+                    while (s.isNotEmpty()) {
+                        var tempNode = s.pop()
+                        r2l.add(tempNode.`val`)
+                    }
+                    r.add(r2l)
+                }
+                d++
+            }
+
+        }
+
+        return r
+    }
+
+}
--- lib/src/main/java/com/example/lib/topics/arrayList/ArrayListDemo.kt
+++ lib/src/main/java/com/example/lib/topics/arrayList/ArrayListDemo.kt
@@ -3,7 +3,8 @@ package com.example.lib.topics.arrayList
 fun main(){
 
     val arrayListOf = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    printArrayList(arrayListOf)
+//    printArrayList(arrayListOf)
+    testArrayListRef()
 }
 
 fun printArrayList(arrayListOf: ArrayList<Int>) {
@@ -11,4 +12,15 @@ fun printArrayList(arrayListOf: ArrayList<Int>) {
     println(".toString: $toString")
     val joinToString = arrayListOf.joinToString(",", "{", "}")
     println(".joinToString : $joinToString")
+}
+
+fun testArrayListRef(){
+
+    val r = arrayListOf<ArrayList<Int>>()
+
+    val l1 = arrayListOf<Int>(1,2,3)
+
+    r.addAll(listOf(l1))
+    l1.clear()
+    println(r)
 }
