fdb04720c73e435396b497ba350ef9895be16d78
vihaan verma <vihaanverma21@gmail.com>
2023-11-18 13:15:54 +0530

    canVisitAllRooms

---
+++ lib/src/main/java/com/example/lib/leetcode/graphs/canVisitAllRooms.kt
@@ -0,0 +1,39 @@
+package com.example.lib.leetcode.graphs
+
+class canVisitAllRooms {
+
+
+    val vHashSet = hashSetOf<Int>()
+    fun dfs(node: Int, hmap: HashMap<Int, List<Int>>) {
+
+        if (vHashSet.contains(node)) {
+            return
+        } else {
+            vHashSet.add(node)
+            val childNodes = hmap.get(node)
+            if (childNodes != null) {
+                for (childNode in childNodes) {
+                    dfs(childNode, hmap)
+                }
+            }
+        }
+    }
+
+    fun canVisitAllRooms(rooms: List<List<Int>>): Boolean {
+
+        val hmap = hashMapOf<Int, List<Int>>()
+
+        // make dfs graph
+        for (roomIv in rooms.withIndex()) {
+            hmap[roomIv.index] = roomIv.value
+        }
+
+        // visit all possible nodes
+        dfs(0, hmap)
+
+        return vHashSet.count() == rooms.size
+        // visitCount == nodeCount ?
+    }
+
+
+}
