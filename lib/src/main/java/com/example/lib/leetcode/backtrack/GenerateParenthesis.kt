b7ac58f06ac80b82b578c8d3de7a5c4560b096d9
vihaan verma <vihaanverma21@gmail.com>
2024-05-26 08:36:22 +0530

    generate paran

--- lib/src/main/java/com/example/lib/leetcode/backtrack/GenerateParenthesis.kt
+++ lib/src/main/java/com/example/lib/leetcode/backtrack/GenerateParenthesis.kt
@@ -1,13 +1,19 @@
 package com.example.lib.leetcode.backtrack1
 
 
-//fun generateParenthesis(n: Int): List<String> {
+//fun generateParenthesis1(n: Int): List<String> {
 //    val ans = ArrayList<String>()
 //    backtrack1(ans, "", 0, 0, n)
 //    return ans
 //}
-
-//fun backtrack1(ans: ArrayList<String>, cur: String, open: Char, close: ArrayList<String>, max: ArrayList<String>) {
+//
+//fun backtrack1(
+//    ans: ArrayList<String>,
+//    cur: String,
+//    open: Char,
+//    close: ArrayList<String>,
+//    max: ArrayList<String>
+//) {
 //    val joinToString = ans.joinToString(",")
 //
 //    println("before len check")
@@ -35,20 +41,64 @@ package com.example.lib.leetcode.backtrack1
 //    }
 //}
 
-// 12th jan 2024
-//fun generateParenthesis(n: Int): List<String> {
-//
-//}
-//
-//
-//fun backtrack1(i: Int, n: Int, tempList: ArrayList<Char>, ansList: ArrayList<String>) {
-//
-//}
+// 16th may 2024
+fun generateParenthesis(n: Int): List<String> {
+    val ansList = arrayListOf<String>()
+    backtrack(n, "",0, 0, ansList)
+    return ansList
+}
+
+
+var tempString: String = ""
+
+fun backtrack(
+    n: Int,
+    tempResult: String,
+    openCount: Int,
+    closeCount: Int,
+    ansList: ArrayList<String>
+) {
+    // base condition
+    if (tempResult.length == n * 2) {
+        println("ans found $tempResult")
+        ansList.add(tempResult+ "")
+        return
+    }
+
+
+    if (openCount > n) {
+        // can't add more (
+        return
+    }
+    if (closeCount > n) {
+        // can't add more )
+        return
+    }
+
+
+    // traversal
+
+//    for (i in 0 until 2) {
+        // logic
+        if (openCount < n) {
+            //foward
+            backtrack(n, "$tempResult(", openCount + 1, closeCount, ansList)
+            //backgward
+            tempResult.dropLast(0)
+        }
+        if (closeCount < n && closeCount < openCount) {
+            backtrack(n, "$tempResult)", openCount, closeCount + 1, ansList)
+            tempResult.dropLast(0)
+        }
+//    }
+
+
+}
 
 fun main() {
     val n = 3
-//    val generateParenthesis = generateParenthesis(3)
-//    val joinToString = generateParenthesis.joinToString(",")
-//    println(joinToString)
+    val generateParenthesis = generateParenthesis(3)
+    val joinToString = generateParenthesis.joinToString(",")
+    println("final result ---> $joinToString")
 }
 
