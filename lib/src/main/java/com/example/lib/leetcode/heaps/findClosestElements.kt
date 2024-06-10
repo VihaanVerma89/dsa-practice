6b97fbc7e003f76ebfe0874b9600b6db31ec9c5c
vihaan verma <vihaanverma21@gmail.com>
2024-06-10 08:11:12 +0530

    findclosestelements

--- lib/src/main/java/com/example/lib/leetcode/heaps/findClosestElements.kt
+++ lib/src/main/java/com/example/lib/leetcode/heaps/findClosestElements.kt
@@ -7,45 +7,7 @@ import java.util.PriorityQueue
 import kotlin.Comparator
 import kotlin.math.absoluteValue
 
-class     class NodeDiff(val value: Int, val diff: Int)
-
-
-val c = Comparator<NodeDiff> { p0, p1 ->
-    if (p1.diff == p0.diff) {
-        p1.value - p0.value
-    } else {
-        p1.diff - p0.diff
-    }
-}
-val pq = PriorityQueue(c)
-
-fun findClosestElements(arr: IntArray, k: Int, x: Int): List<Int> {
-
-    val ansList = ArrayList<Int>()
-
-    // generate value diffs
-    var diff = Int.MIN_VALUE
-    for (num in arr) {
-        diff = num - x
-        val nodeDiff = NodeDiff(num, diff.absoluteValue)
-        pq.add(nodeDiff)
-    }
-
-
-    if (pq.size > k) {
-        while (pq.size != k) {
-            pq.remove()
-        }
-    }
-
-    // pq size <=k
-    while (pq.isNotEmpty()) {
-        ansList.add(pq.remove().value)
-    }
-
-    ansList.sort()
-    return ansList
-} {
+class findClosestElements {
 
     class NodeDiff(val value: Int, val diff: Int)
 
