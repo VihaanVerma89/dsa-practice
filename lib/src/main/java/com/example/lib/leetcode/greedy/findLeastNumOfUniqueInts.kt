27cf5921696c698cabc89c3ea12ee23ce8e8af63
vihaan verma <vihaanverma21@gmail.com>
2024-07-01 08:49:15 +0530

    TC of findLeastNumOfUniqueInts

--- lib/src/main/java/com/example/lib/leetcode/greedy/findLeastNumOfUniqueInts.kt
+++ lib/src/main/java/com/example/lib/leetcode/greedy/findLeastNumOfUniqueInts.kt
@@ -5,19 +5,22 @@ import kotlin.math.min
 
 class findLeastNumOfUniqueInts {
 
+    // TC - O(n+mlogm+klogk)
+    // considering n = m and k = n
+    // O(n+nlogn+nlogn)
+    // o( n + 2nlogn )
+    // dominating term nlogn
+    // final complexity = o( n log n )
     fun findLeastNumOfUniqueInts(arr: IntArray, k: Int): Int {
 
         if (arr.isEmpty()) {
             return 0
         }
 
-        // create min heap
-        class Item(val value: Int, var freq: Int)
 
-        val minHeap = PriorityQueue<Item> { p0, p1 -> p0.freq - p1.freq }
-
+        // create a hmap
+        // Time Complexity - o(n)
         val hmap = HashMap<Int, Int>()
-
         for (item in arr) {
             if (hmap.contains(item)) {
                 val freq = hmap.get(item)
@@ -27,11 +30,18 @@ class findLeastNumOfUniqueInts {
             }
         }
 
+        // create min heap
+        // m unique elements
+        // TC - o(m log m)
+        class Item(val value: Int, var freq: Int)
+
+        val minHeap = PriorityQueue<Item> { p0, p1 -> p0.freq - p1.freq }
         for (entry in hmap) {
             minHeap.add(Item(entry.key, entry.value))
         }
 
         // remove k elements from heap
+        // TC - o( k log k)
         var i = k
         while (minHeap.isNotEmpty() && i > 0) {
             val top = minHeap.peek()
