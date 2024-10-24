ae4592aa36c230dbb156c36f8cfa398aa03a4381
vihaan verma <vihaanverma21@gmail.com>
2024-10-24 18:50:03 +0530

    findBestSubarray

--- lib/src/main/java/com/example/lib/leetcode/arrays/slidingwindow/findBestSubarray.kt
+++ lib/src/main/java/com/example/lib/leetcode/arrays/slidingwindow/findBestSubarray.kt
@@ -2,7 +2,29 @@ package com.example.lib.leetcode.arrays.slidingwindow
 
 class findBestSubarray {
 
+
     fun findBestSubarray(nums: IntArray, k: Int): Int {
+
+        var s = 0
+        var sum = 0
+        var ans = 0
+
+        for (e in 0 until nums.size) {
+
+            sum += nums[e]
+
+            if (e > k) {
+                sum -= nums[e - k]
+            }
+
+            ans = maxOf(ans, sum)
+        }
+
+        return ans
+    }
+
+
+    fun findBestSubarray1(nums: IntArray, k: Int): Int {
         var wSum = 0
         var ans = 0
 
@@ -13,7 +35,7 @@ class findBestSubarray {
         for (i in k until nums.size) {
             wSum += nums[i]
             wSum -= nums[i - k]
-            ans = maxOf(ans,wSum)
+            ans = maxOf(ans, wSum)
         }
 
         return ans
