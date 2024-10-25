b5a58600ddb8bc939969fc9d8bae180fdeac700e
vihaan verma <vihaanverma21@gmail.com>
2024-10-25 11:45:33 +0530

    findmaxavg

---
+++ lib/src/main/java/com/example/lib/leetcode/arrays/findMaxAverage.kt
@@ -0,0 +1,23 @@
+package com.example.lib.leetcode.arrays
+
+class findMaxAverage {
+
+    fun findMaxAverage(nums: IntArray, k: Int): Double {
+
+        var sum = 0
+        var ans = 0
+
+        for (e in 0 until k) {
+            sum += nums[e]
+        }
+
+        ans = sum
+        for (e in k until nums.size) {
+            sum += nums[e]
+            sum -= nums[e - k]
+            ans = maxOf(ans, sum)
+        }
+
+        return ans / k.toDouble()
+    }
+}
