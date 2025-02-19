b9fb2957d578bdbaded6fbfc26aabf1e3d0b1353
vihaan <vihaanverma21@gmail.com>
2025-02-19 20:19:57 +0530

    graphdemo

--- lib/src/main/java/com/example/lib/leetcode/graphs/GraphDemo.kt
+++ lib/src/main/java/com/example/lib/leetcode/graphs/GraphDemo.kt
@@ -1,35 +1,66 @@
 package com.example.lib.leetcode.graphs
 
-import com.example.lib.Solution
-
 class GraphDemo {
 
-    val graph = mutableMapOf<Int, MutableList<Int>>()
+    val graphMap = mutableMapOf<Int, MutableList<Int>>()
 
     // map.computeIfAbsent
-    fun addEdge(source: Int, destination: Int) {
-        graph.computeIfAbsent(source) { mutableListOf() }.add(destination)
-        graph.computeIfAbsent(destination) { mutableListOf() }.add(source)
+    fun addEdgeInMap(source: Int, destination: Int) {
+        graphMap.computeIfAbsent(source) { mutableListOf() }.add(destination)
+        graphMap.computeIfAbsent(destination) { mutableListOf() }.add(source)
     }
 
     // for ((k,v) in map)
-    fun printGraph() {
+    fun printMapGraph() {
 
-        for ((key, value) in graph) {
+        for ((key, value) in graphMap) {
             println("$key -> $value ")
         }
 
     }
 
+
+}
+
+class GraphMatrix(private val size: Int) {
+
+    private val matrix = Array(size) { IntArray(size) }
+
+
+    fun addEdge(source: Int, destination: Int) {
+        matrix[source][destination] = 1
+        matrix[destination][source] = 1
+    }
+
+    fun printMatrix() {
+        for (intArray in matrix) {
+            println(intArray.contentToString())
+        }
+    }
 }
 
 fun main() {
 
+    mapDemo()
+    println()
+    matrixDemo()
+
+}
+
+fun mapDemo() {
     val graph = GraphDemo()
-    graph.addEdge(1, 2)
+    graph.addEdgeInMap(1, 2)
+    graph.addEdgeInMap(1, 3)
+    graph.addEdgeInMap(2, 4)
+    graph.addEdgeInMap(3, 4)
+    graph.printMapGraph()
+}
+
+fun matrixDemo() {
+    val graph = GraphMatrix(5)
+    graph.addEdge(0, 1)
+    graph.addEdge(0, 2)
     graph.addEdge(1, 3)
     graph.addEdge(2, 4)
-    graph.addEdge(3, 4)
-    graph.printGraph()
-
+    graph.printMatrix()
 }
