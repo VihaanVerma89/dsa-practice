1c0cf6849587044c5fd6ead556dfda2d10a42aba
vihaan <vihaanverma21@gmail.com>
2025-03-01 13:24:43 +0530

    findSmallestSetOfVertices

--- lib/src/main/java/com/example/lib/kotlinCode.kt
+++ lib/src/main/java/com/example/lib/kotlinCode.kt
@@ -4,46 +4,8 @@ import java.util.LinkedList
 
 class Solution {
 
-    fun canVisitAllRooms(rooms: List<List<Int>>): Boolean{
-
-        val graph = mutableMapOf<Int, MutableList<Int>>()
-
-        var i = 0
-        for(room in rooms){
-
-            graph.getOrPut(i){ mutableListOf() }.addAll(room)
-            i++
-        }
-
-        val visited = hashSetOf<Int>()
-
-        fun dfs(room: Int){
-            if(visited.contains(room)){
-
-                // already visited
-                return
-            }
-
-            visited.add(room)
-
-            val keys = graph.getOrDefault(room, emptyList())
-
-            if(keys.isEmpty()){
-                return
-            }
 
 
-            for(key in keys){
-                dfs(key)
-            }
-
-        }
-
-        dfs(0)
-
-        return rooms.size == visited.size
-
-    }
 
 
 
--- lib/src/main/java/com/example/lib/leetcode/graphs/findSmallestSetOfVertices.kt
+++ lib/src/main/java/com/example/lib/leetcode/graphs/findSmallestSetOfVertices.kt
@@ -2,50 +2,40 @@ package com.example.lib.leetcode.graphs
 
 class findSmallestSetOfVertices {
 
-    val visited = hashSetOf<Int>()
-    fun dfs(n: Int, hmap: HashMap<Int, ArrayList<Int>>) {
-        if (visited.contains(n)) {
+    fun findSmallestSetOfVertices(n: Int, edges:List<List<Int>>): List<Int> {
 
-        } else {
-            visited.add(n)
-            hmap[n]?.apply {
-                for (node in hmap[n]!!) {
-                    dfs(node, hmap)
-                }
+        val hSet = hashSetOf<Int>()
+        for(i in 0 until n){
+            hSet.add(i)
+        }
+
+        for(edge in edges){
+            var s = edge[0]
+            var e = edge[1]
+
+            if(hSet.contains(e)){
+                hSet.remove(e)
             }
         }
+
+
+        val result = mutableListOf<Int>()
+        for(value in hSet){
+
+            result.add(value)
+        }
+        return result
+
     }
 
-    fun findSmallestSetOfVertices(n: Int, edges: List<List<Int>>): List<Int> {
+    fun findSmallestSetOfVerticesAI(n: Int, edges: List<List<Int>>): List<Int> {
+        val hSet = mutableSetOf<Int>().apply { addAll(0 until n) }
 
-        // prepare the node -> connected node map
-
-        val hmap = hashMapOf<Int, ArrayList<Int>>()
         for (edge in edges) {
-            val start = edge[0]
-            val end = edge[1]
-
-            if (hmap.contains(start)) {
-                hmap[start]?.add(end)
-            } else {
-                hmap[start] = arrayListOf<Int>().apply { add(end) }
-            }
-
+            hSet.remove(edge[1])
         }
 
-        // dfs
-        val result = arrayListOf<Int>()
-        for (i in 0 until n) {
-            if (visited.contains(i)) {
-            } else {
-                if (hmap.contains(i)) {
-                    dfs(i, hmap)
-                    result.add(i)
-                }
-            }
-        }
-
-        return result
+        return hSet.toList()
     }
 }
 
